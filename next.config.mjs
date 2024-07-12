21aeb1a8c6d8335ffbc985faf4315601d1e54e4c
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-07-12 10:08:19 +0530

    small change in config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   basePath: isProd ? "/KCC-NEXT" : "",
   assetPrefix: isProd ? "/KCC-NEXT/" : "",
-  output: isProd ? "export" : "", // <=== enables static exports
+  output: isProd ? "export" : undefined, // <=== enables static exports
   images: {
     remotePatterns: [
       {
