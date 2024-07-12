f897763a030e8503b67c237565757c3cc7ad3bab
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-07-12 13:27:54 +0530

    added new pages and config

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,10 +60,96 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://cdn.jsdelivr.net/npm/pagedone@1.1.2/src/css/pagedone.css"
+          rel="stylesheet"
+        />
+        <meta
+          name="google-site-verification"
+          content="FD8caxnx0zUzRFgtOk1O_cZ_UShPj3WxN3-cLQVBpO0"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `document.documentElement.classList.add('js')`,
+          }}
+        />
+        <Script
+          id=""
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `{
+              "@context": "https://schema.org",
+              "@type": "Event",
+              "name": "Kerala Campus Conference 2024",
+              "startDate": "2024-08-23T00:00+05:30",
+              "endDate": "2024-08-26T23:59+05:30",
+              "eventAttendanceMode": "https://schema.org/OfflineEventAttendanceMode",
+              "eventStatus": "https://schema.org/EventScheduled",
+              "location": {
+                "@type": "Place",
+                "name": "Rajagiri College of Social Sciences",
+                "address": {
+                  "@type": "PostalAddress",
+                  "streetAddress": "Rajagiri College of Social Sciences",
+                  "addressLocality": "Kalamassery",
+                  "postalCode": "683104",
+                  "addressRegion": "Kerala",
+                  "addressCountry": "India"
+                }
+              },
+              "image": [
+                "https://kcc24.live/assets/hero.webp"
+              ],
+              "description": "Join us for the Kerala Campus Conference 2024. Proclaim from the rooftops!",
+              "offers": {
+                "@type": "Offer",
+                "url": "https://kcc24.live/registration",
+                "price": "2000",
+                "priceCurrency": "INR",
+                "availability": "https://schema.org/InStock",
+                "validFrom": "2024-07-01T00:00+05:30"
+              },
+              "performer": {
+                "@type": "PerformingGroup",
+                "name": "Jesus Youth"
+              }
+            }`,
+          }}
+        />
+      </head>
       <body
         className={`bg-gray-900 text-white bg-cover ${inter.className}`}
         style={{ backgroundImage: "url('./assets/bg.webp')" }}
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-070KWDF5CS"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-070KWDF5CS');
+          `}
+        </Script>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PGMF54GR');
+          `}
+        </Script>
         {children}
       </body>
     </html>
